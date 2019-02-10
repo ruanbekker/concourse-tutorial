bf1be45c5bb80e8e56b484eb9f2fde66a679161b
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-02-10 15:17:59 +0200

    add resources

--- 07-task-outputs-to-inputs-pipeline/create_some_files.sh
+++ 07-task-outputs-to-inputs-pipeline/create_some_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir -p some-files
-echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/file1.txt
-echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/file2.txt
-echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/file3.txt
-echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/file4.txt
+echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/data1.txt
+echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/data2.txt
+echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/data3.txt
+echo "$(cat /proc/sys/kernel/random/uuid)" > some-files/data4.txt
 
 ls some-files/*
