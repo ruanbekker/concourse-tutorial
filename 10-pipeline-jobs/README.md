d7b2dbc5e4f2c90e44c10f9ba614eb504e920ad6
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-02-10 20:24:59 +0200

    add lesson 10

---
+++ 10-pipeline-jobs/README.md
@@ -0,0 +1,3 @@
+## 10 - Pipeline Jobs
+
+Building a Pipeline with more than one job, so that one job passing results to another job, upon success.
---
+++ 10-pipeline-jobs/bump_timestamp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+user_email="ruan.ru.bekker@gmail.com"
+user_name="Ruan Bekker"
+
+git clone concourse-date updated-timestamp
+
+cd updated-timestamp
+echo "The date: $(date)" > 10-pipeline-jobs/bumpme
+
+git config --global user.email "${user_email}"
+git config --global user.name "${user_name}"
+
+git add .
+git commit -m "bumped date"
