f7f0ac5b2a1d15ba2809755c453e04466f986c63
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-02-10 17:40:18 +0200

    change path of bumpme file

--- 08-publishing-outputs/bump_timestamp.sh
+++ 08-publishing-outputs/bump_timestamp.sh
@@ -7,7 +7,7 @@ user_name="Ruan Bekker"
 git clone concourse-date updated-timestamp
 
 cd updated-timestamp
-echo "The date: $(date)" > bumpme
+echo "The date: $(date)" > 08-publishing-outputs/bumpme
 
 git config --global user.email "${user_email}"
 git config --global user.name "${user_name}"
